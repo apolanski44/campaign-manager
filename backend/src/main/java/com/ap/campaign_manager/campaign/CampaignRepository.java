9f1809fa9413a5ee806872cac1f51e95cc58f0ce
Adrian <apolanski1234@gmail.com>
2026-03-25 18:09:52 +0100

    Add CampaignRepository

---
+++ backend/src/main/java/com/ap/campaign_manager/campaign/CampaignRepository.java
@@ -0,0 +1,8 @@
+package com.ap.campaign_manager.campaign;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CampaignRepository extends JpaRepository<Campaign, UUID> {
+}
