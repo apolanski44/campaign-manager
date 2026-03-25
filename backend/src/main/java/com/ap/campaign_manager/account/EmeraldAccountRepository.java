3b1497d68c65926e3c975907ae258f55d91f0271
Adrian <apolanski1234@gmail.com>
2026-03-25 23:21:40 +0100

    Remove redundant method, add custom method for retrieving balance

--- backend/src/main/java/com/ap/campaign_manager/account/EmeraldAccountRepository.java
+++ backend/src/main/java/com/ap/campaign_manager/account/EmeraldAccountRepository.java
@@ -1,10 +1,13 @@
 package com.ap.campaign_manager.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface EmeraldAccountRepository extends JpaRepository<EmeraldAccount, UUID> {
-    Optional<EmeraldAccount> findById(UUID id);
+    @Query("SELECT e.balance FROM EmeraldAccount e WHERE e.id = :id")
+    Optional<BigDecimal> getBalanceById(UUID id);
 }
