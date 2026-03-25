1842353a3f3c20a505576bc7774c47f48b493ef2
Adrian <apolanski1234@gmail.com>
2026-03-25 13:32:43 +0100

    Initial commit

---
+++ backend/src/main/java/com/ap/campaign_manager/CampaignManager.java
@@ -0,0 +1,13 @@
+package com.ap.campaign_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManager.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ap/campaign_manager/CampaignManagerTests.java
@@ -0,0 +1,13 @@
+package com.ap.campaign_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignManagerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
