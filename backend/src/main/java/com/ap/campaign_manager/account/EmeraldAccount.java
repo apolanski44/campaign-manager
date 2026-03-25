4c07ab7a94ba33781dcf84dbdff215f82f450909
Adrian <apolanski1234@gmail.com>
2026-03-25 17:28:27 +0100

    Add Account entity

---
+++ backend/src/main/java/com/ap/campaign_manager/account/EmeraldAccount.java
@@ -0,0 +1,25 @@
+package com.ap.campaign_manager.account;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmeraldAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Version
+    private Long version;
+}
