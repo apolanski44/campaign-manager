240cffe4d5294039f0c47cfd38a31e0767015aea
Adrian <apolanski1234@gmail.com>
2026-03-25 23:51:22 +0100

    Add account controller with get balance endpoint

---
+++ backend/src/main/java/com/ap/campaign_manager/account/EmeraldAccountController.java
@@ -0,0 +1,19 @@
+package com.ap.campaign_manager.account;
+
+import com.ap.campaign_manager.campaign.dto.AccountBalanceResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/account")
+public class EmeraldAccountController {
+    private final EmeraldAccountService emeraldAccountService;
+
+    @GetMapping("/balance")
+    public AccountBalanceResponseDto getBalance() {
+        return emeraldAccountService.getAccountBalance();
+    }
+}
